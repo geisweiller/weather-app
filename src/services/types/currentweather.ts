334f159bec358ed1910ccdf471282d41c278ded2
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2024-01-21 19:29:14 -0300

    feat(services): mock + type update

--- src/services/types/currentweather.ts
+++ src/services/types/currentweather.ts
@@ -27,4 +27,75 @@ interface CurrentWeatherService {
       }
     ];
   };
+  minutely: {
+    dt: number;
+    precipitation: number;
+  }[];
+  hourly: {
+    dt: number;
+    temp: number;
+    feels_like: number;
+    pressure: number;
+    humidity: number;
+    dew_point: number;
+    uvi: number;
+    clouds: number;
+    visibility: number;
+    wind_speed: number;
+    wind_deg: number;
+    wind_gust: number;
+    weather: {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }[];
+    pop: number;
+  }[];
+  daily: {
+    dt: number;
+    sunrise: number;
+    sunset: number;
+    moonrise: number;
+    moonset: number;
+    moon_phase: number;
+    summary: string;
+    temp: {
+      day: number;
+      min: number;
+      max: number;
+      night: number;
+      eve: number;
+      morn: number;
+    };
+    feels_like: {
+      day: number;
+      night: number;
+      eve: number;
+      morn: number;
+    };
+    pressure: number;
+    humidity: number;
+    dew_point: number;
+    wind_speed: number;
+    wind_deg: number;
+    wind_gust: number;
+    weather: {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }[];
+    clouds: number;
+    pop: number;
+    uvi: number;
+  }[];
+  alerts: {
+    sender_name: string;
+    event: string;
+    start: number;
+    end: number;
+    description: string;
+    tags: [];
+  }[];
 }
