5b7ba96a90b2528b3ac8fce54d8239caedd50a28
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2024-01-22 09:52:00 -0300

    feat(services): urls file

---
+++ src/services/urls/urls.ts
@@ -0,0 +1,7 @@
+const geocodingUrl = (city: string, apiKey: string) =>
+  `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
+
+const weatherUrl = (lat: number, lon: number, unit: string, apiKey: string) =>
+  `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=${unit}&&appid=${apiKey}`;
+
+export { geocodingUrl, weatherUrl };
