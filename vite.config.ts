6cc55fd26ca57fec3558263dc8e5d9149c2dfd71
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2024-01-22 22:06:46 -0300

    fix(vite): fix config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/weather-app/",
   define: {
     "process.env": process.env,
-    base: "/weather-app/",
   },
 });
