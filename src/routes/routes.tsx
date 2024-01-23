0c753ce851bf3a7a87d2f5b31be9d5dbce5e762d
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2024-01-22 21:52:57 -0300

    feat(routes): add route basename

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,17 +2,22 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { Locations, NotFound, Weather } from "../pages";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Locations />,
+    },
+    {
+      path: "/:location",
+      element: <Weather />,
+      errorElement: <NotFound />,
+    },
+  ],
   {
-    path: "/",
-    element: <Locations />,
-  },
-  {
-    path: "/:location",
-    element: <Weather />,
-    errorElement: <NotFound />,
-  },
-]);
+    basename: "/weather-app",
+  }
+);
 
 const Routes = () => {
   return <RouterProvider router={router} />;
